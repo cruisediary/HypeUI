e60a68aeb8bd74335f6199fa427c6e8be46c6d6e
cruz <cruzdiary@gmail.com>
2025-09-09 23:06:49 +0900

    Add UITextField+TextInput extension

---
+++ Sources/HypeUI/UITextField+TextInput.swift
@@ -0,0 +1,78 @@
+//
+//  Copyright 2022 Hyperconnect Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+
+// MARK: - UITextField (SwiftUI Style)
+
+public extension UITextField {
+    
+    /// Sets the keyboard type for the text field
+    /// - Parameter type: The keyboard type to use
+    /// - Returns: Modified text field
+    func keyboardType(_ type: UIKeyboardType) -> Self {
+        self.keyboardType = type
+        return self
+    }
+    
+    /// Sets whether to disable autocorrection for the text field
+    /// - Parameter disabled: Whether autocorrection is disabled
+    /// - Returns: Modified text field
+    func autocorrectionDisabled(_ disabled: Bool = true) -> Self {
+        self.autocorrectionType = disabled ? .no : .yes
+        return self
+    }
+    
+    /// Sets the text field's text content type for autofill
+    /// - Parameter type: The text content type
+    /// - Returns: Modified text field
+    func textContentType(_ type: UITextContentType?) -> Self {
+        self.textContentType = type
+        return self
+    }
+    
+    /// Sets the text field style similar to SwiftUI's textFieldStyle
+    /// - Parameter style: The border style to apply
+    /// - Returns: Modified text field
+    func textFieldStyle(_ style: UITextField.BorderStyle) -> Self {
+        self.borderStyle = style
+        return self
+    }
+    
+    /// Sets the submit label for the return key
+    /// - Parameter label: The return key type
+    /// - Returns: Modified text field
+    func submitLabel(_ label: UIReturnKeyType) -> Self {
+        self.returnKeyType = label
+        return self
+    }
+    
+    /// Sets the text capitalization style
+    /// - Parameter style: The capitalization style
+    /// - Returns: Modified text field
+    func textInputAutocapitalization(_ style: UITextAutocapitalizationType) -> Self {
+        self.autocapitalizationType = style
+        return self
+    }
+    
+    /// Sets whether the text field is a secure entry (password field)
+    /// - Parameter isSecure: Whether to obscure the text
+    /// - Returns: Modified text field
+    func secureField(_ isSecure: Bool = true) -> Self {
+        self.isSecureTextEntry = isSecure
+        return self
+    }
+}
---
+++ Tests/HypeUITests/UITextField+TextInputTests.swift
@@ -0,0 +1,133 @@
+//
+//  Copyright 2022 Hyperconnect Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import XCTest
+import UIKit
+
+// MARK: - UITextField+TextInputTests
+
+@testable import HypeUI
+final class UITextFieldTextInputTests: XCTestCase {
+    
+    func testKeyboardType() {
+        // given
+        let textField = UITextField()
+        
+        // when
+        let modifiedTextField = textField.keyboardType(.emailAddress)
+        
+        // then
+        XCTAssertEqual(modifiedTextField, textField)
+        XCTAssertEqual(textField.keyboardType, .emailAddress)
+    }
+    
+    func testAutocorrectionDisabled() {
+        // given
+        let textField = UITextField()
+        
+        // when
+        let modifiedTextField = textField.autocorrectionDisabled()
+        
+        // then
+        XCTAssertEqual(modifiedTextField, textField)
+        XCTAssertEqual(textField.autocorrectionType, .no)
+    }
+    
+    func testAutocorrectionEnabled() {
+        // given
+        let textField = UITextField()
+        
+        // when
+        let modifiedTextField = textField.autocorrectionDisabled(false)
+        
+        // then
+        XCTAssertEqual(modifiedTextField, textField)
+        XCTAssertEqual(textField.autocorrectionType, .yes)
+    }
+    
+    func testTextContentType() {
+        // given
+        let textField = UITextField()
+        
+        // when
+        let modifiedTextField = textField.textContentType(.emailAddress)
+        
+        // then
+        XCTAssertEqual(modifiedTextField, textField)
+        XCTAssertEqual(textField.textContentType, .emailAddress)
+    }
+    
+    func testTextFieldStyle() {
+        // given
+        let textField = UITextField()
+        
+        // when
+        let modifiedTextField = textField.textFieldStyle(.roundedRect)
+        
+        // then
+        XCTAssertEqual(modifiedTextField, textField)
+        XCTAssertEqual(textField.borderStyle, .roundedRect)
+    }
+    
+    func testSubmitLabel() {
+        // given
+        let textField = UITextField()
+        
+        // when
+        let modifiedTextField = textField.submitLabel(.done)
+        
+        // then
+        XCTAssertEqual(modifiedTextField, textField)
+        XCTAssertEqual(textField.returnKeyType, .done)
+    }
+    
+    func testTextInputAutocapitalization() {
+        // given
+        let textField = UITextField()
+        
+        // when
+        let modifiedTextField = textField.textInputAutocapitalization(.words)
+        
+        // then
+        XCTAssertEqual(modifiedTextField, textField)
+        XCTAssertEqual(textField.autocapitalizationType, .words)
+    }
+    
+    func testSecureField() {
+        // given
+        let textField = UITextField()
+        
+        // when
+        let modifiedTextField = textField.secureField()
+        
+        // then
+        XCTAssertEqual(modifiedTextField, textField)
+        XCTAssertTrue(textField.isSecureTextEntry)
+    }
+    
+    func testSecureFieldDisabled() {
+        // given
+        let textField = UITextField()
+        textField.isSecureTextEntry = true
+        
+        // when
+        let modifiedTextField = textField.secureField(false)
+        
+        // then
+        XCTAssertEqual(modifiedTextField, textField)
+        XCTAssertFalse(textField.isSecureTextEntry)
+    }
+}
